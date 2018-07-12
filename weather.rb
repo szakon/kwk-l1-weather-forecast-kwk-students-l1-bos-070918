370313830d40cf23fc89b786a7a774718935ba76
szakon <sarahzakon@gmail.com>
2018-07-12 14:54:40 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -13,10 +13,14 @@ def location(weather)
 	
 end
 
-# 2. Using the location method as a guide, write a  method called "description" so it returns the description from the weather hash. 
+# 2. Using the location method as a guide, write a  method called "description" so it returns the description from the weather hash.
+def descriptiona(weather)
+  weathe[:description]
+end
 
 
 # 3. Using the methods above and string interpolation, update the method below so that it returns a string that describes the current weather - for example: "The weather in New York is Partly Cloudy. The temperature is 66.1 degrees Farenheit." HINT: the method should RETURN the string, not print it to the terminal. 
+
 
 
 #4. Write a method that takes in the weather hash as an argument. 
