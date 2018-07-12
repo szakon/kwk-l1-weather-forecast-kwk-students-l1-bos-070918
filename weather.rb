e0e61b680863a211f0c5c0257c2b924458ef70a5
szakon <sarahzakon@gmail.com>
2018-07-12 15:11:57 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -19,10 +19,13 @@ def description(weather)
 end
 
 
+
 # 3. Using the methods above and string interpolation, update the method below so that it returns a string that describes the current weather - for example: "The weather in New York is Partly Cloudy. The temperature is 66.1 degrees Farenheit." HINT: the method should RETURN the string, not print it to the terminal.
 def weather_string(weather)
   weather.each do |key, value|
-    puts "the weather in #{location} is #{description}"  
+    puts "the weather in #{location(weather)} is #{description(weather)} the temperature is #{weather[:temperature_farenheit]
+end
+  
 
 
 
